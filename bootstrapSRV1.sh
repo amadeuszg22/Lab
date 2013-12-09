2fb0999905b288f71a2e999b3f0347711dba5177
amadeuszg22 <amadeusz.grabowski@gmail.com>
2013-12-09 20:08:27 +0100

    Update bootstrapSRV1.sh

--- bootstrapSRV1.sh
+++ bootstrapSRV1.sh
@@ -13,17 +13,52 @@ sudo apt-get install php5-memcache -y
 echo "system enables modules"
 sudo a2enmod mem_cache
 echo "system restart apache2 service"
-echo "SRV1" > /etc/hostname
-touch /var/www/test.php
-chmod a+rw /var/www/test.php
-echo "
-<?php
-phpinfo();
-?>">/var/www/test.php
+sudo echo "<VirtualHost *:80>
+	ServerAdmin webmaster@localhost
+
+	DocumentRoot /webfiles/
+	<Directory />
+		Options FollowSymLinks
+		AllowOverride None
+	</Directory>
+	<Directory /webfiles/>
+		Options Indexes FollowSymLinks MultiViews
+		AllowOverride None
+		Order allow,deny
+		allow from all
+	</Directory>
+
+	ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
+	<Directory "/usr/lib/cgi-bin">
+		AllowOverride None
+		Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
+		Order allow,deny
+		Allow from all
+	</Directory>
+
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+
+	# Possible values include: debug, info, notice, warn, error, crit,
+	# alert, emerg.
+	LogLevel warn
+
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+    Alias /doc/ "/usr/share/doc/"
+    <Directory "/usr/share/doc/">
+        Options Indexes MultiViews FollowSymLinks
+        AllowOverride None
+        Order deny,allow
+        Deny from all
+        Allow from 127.0.0.0/255.0.0.0 ::1/128
+    </Directory>
+
+</VirtualHost>">/etc/apache2/sites-enabled/NMC
+rm /etc/apache2/sites-enabled/000-default
+echo "SRV" > /etc/hostname
 sudo echo "127.0.0.1 NMC"> /etc/hosts
 sudo service apache2 restart
 sudo apt-get install portmap nfs-common -y
 sudo mkdir /webfiles
 sudo echo "192.168.10.10:/var/www/ /webfiles nfs rsize=8192,wsize=8192,timeo=14,intr">>/etc/fstab
-sudo chmod a+rw webfiles/
 sudo reboot
